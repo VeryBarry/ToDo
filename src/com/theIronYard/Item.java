a6693a4e741bfa38c38dd693c9d56f1bdc03edca
VeryBarry <barry.a.daniels@gmail.com>
2016-09-19 11:18:09 -0400

    todo list

---
+++ src/com/theIronYard/Item.java
@@ -0,0 +1,14 @@
+package com.theIronYard;
+
+/**
+ * Created by VeryBarry on 9/19/16.
+ */
+public class Item {
+    String text;
+    boolean isDone;
+
+    public Item(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
